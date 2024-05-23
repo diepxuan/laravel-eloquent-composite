3f4986b6c8875def405e33922b6fbbb4c006300e
Trần Ngọc Đức <caothu91@gmail.com>
2024-05-23 16:38:01 +0700

    init package

---
+++ src/CompositeModel.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * @copyright  © 2019 Dxvn, Inc.
+ *
+ * @author     Tran Ngoc Duc <ductn@diepxuan.com>
+ * @author     Tran Ngoc Duc <caothu91@gmail.com>
+ *
+ * @lastupdate 2024-05-23 11:51:26
+ */
+
+namespace Diepxuan\Simba\SModel;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CompositeModel extends Model
+{
+    use HasCompositePrimaryKey;
+
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = ['id'];
+}
