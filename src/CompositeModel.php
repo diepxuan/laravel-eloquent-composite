eed429e2497795c8e5dece160d3aa2c61a4fb59d
diepxuan <ductn@diepxuan.com>
2024-07-04 10:05:30 +0000

    Create module.yml

--- src/CompositeModel.php
+++ src/CompositeModel.php
@@ -8,12 +8,12 @@ declare(strict_types=1);
  * @author     Tran Ngoc Duc <ductn@diepxuan.com>
  * @author     Tran Ngoc Duc <caothu91@gmail.com>
  *
- * @lastupdate 2024-05-23 17:39:01
+ * @lastupdate 2024-05-23 17:35:38
  */
 
 namespace Diepxuan\Database\Eloquent;
 
-use Diepxuan\Database\Eloquent\Traits\HasCompositePrimaryKey;
+use Database\Eloquent\Traits\HasCompositePrimaryKey;
 use Illuminate\Database\Eloquent\Model;
 
 class CompositeModel extends Model
